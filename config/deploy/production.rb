55ce90cdb9758e22fb417a20a1f6e6d94c513ed5
activeliang <activeliang@gmail.com>
2017-03-30 23:33:00 +0800

    for demo

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "172.104.33.191", user: "equibhub-test", roles: %w{app db web}, my_property: :my_value
+server "172.104.33.191", user: "apps", roles: %w{app db web}, my_property: :my_value
 
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
